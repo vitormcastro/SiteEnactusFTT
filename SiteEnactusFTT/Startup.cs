8b43f0149f58d8a734f3fecfce363f8a94000c1d
vitormcastro <vitormcastro@outlook.com>
2019-07-31 22:58:12 -0300

    Add project files.

---
+++ SiteEnactusFTT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteEnactusFTT.Startup))]
+namespace SiteEnactusFTT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
